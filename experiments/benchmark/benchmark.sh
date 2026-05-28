be0d03eb35ccde4f209983f423ac67017f15aa49
Roman Kazachkov <roman.kazachkov1@gmail.com>
2026-05-29 00:10:12 +0300

    fix: make experiment scripts portable

--- experiments/benchmark/benchmark.sh
+++ experiments/benchmark/benchmark.sh
@@ -8,29 +8,41 @@ ARCHIVER="$SCRIPT_DIR/../../build/huff"
 DATA_DIR="$SCRIPT_DIR/../data"
 RESULTS="$SCRIPT_DIR/result.csv"
 
+file_size() {
+    if stat -f%z "$1" >/dev/null 2>&1; then
+        stat -f%z "$1"
+    else
+        stat -c%s "$1"
+    fi
+}
+
+now_ns() {
+    python3 -c 'import time; print(time.time_ns())'
+}
+
 echo "file,type,size,compress_time,decompress_time,ratio" > "$RESULTS"
 
 for file in "$DATA_DIR"/*/*
 do
     type=$(basename "$(dirname "$file")")
     name=$(basename "$file")
-    size=$(stat -f%z "$file")
+    size=$(file_size "$file")
 
     for run in {1..10}
     do
-        start=$(date +%s%N)
+        start=$(now_ns)
         "$ARCHIVER" c "$file" tmp.huff
-        end=$(date +%s%N)
+        end=$(now_ns)
 
         compress_time=$(echo "($end-$start)/1000000000" | bc -l)
 
-        start=$(date +%s%N)
+        start=$(now_ns)
         "$ARCHIVER" d tmp.huff tmp.out
-        end=$(date +%s%N)
+        end=$(now_ns)
 
         decompress_time=$(echo "($end-$start)/1000000000" | bc -l)
 
-        compressed_size=$(stat -f%z tmp.huff)
+        compressed_size=$(file_size tmp.huff)
 
         ratio=$(echo "$compressed_size/$size" | bc -l)
 
